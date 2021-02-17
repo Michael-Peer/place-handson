5622c9af8fabe44002b41ff60e549d2f6df602d0
Yaniv <soudneko@icloud.com>
2021-02-17 18:01:17 +0200

    weather div

--- js/map-controller.js
+++ js/map-controller.js
@@ -13,7 +13,14 @@ function getWeather() {
         })
 }
 
-function renderWeather(weather) {}
+function renderWeather(weather) {
+    const currLocationWeather = weather
+    console.log('currLocationWeather:', currLocationWeather.description)
+    var strHtml = `
+         <h2>${weather.main}<h2><p>${weather.description}</p>`
+
+    document.querySelector('.weather-container').innerHTML = strHtml
+}
 mapService.getLocs()
     .then(locs => console.log('locs', locs))
 window.onload = () => {
@@ -81,9 +88,19 @@ function initMap(lat = 32.0749831, lng = 34.9120554) {
 }
 
 function addMarker(loc) {
+    const svgMarker = {
+        path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+        fillColor: "blue",
+        fillOpacity: 0.6,
+        strokeWeight: 0,
+        rotation: 0,
+        scale: 2,
+        anchor: new google.maps.Point(15, 30),
+    };
     var marker = new google.maps.Marker({
         position: loc,
         map: gMap,
+        icon: svgMarker,
         title: 'Hello World!'
     });
     return marker;
